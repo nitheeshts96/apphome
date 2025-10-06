835abb24f869ec4c97d462d6a89dbab4bb64b4c2
Nitheesh TS <32710049+nitheeshts96@users.noreply.github.com>
2025-10-06 13:03:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View, } from 'react-native';
+
+// You can import supported modules from npm
+import { useRef, useEffect } from 'react-native';
+
+// or any files within the Snack
+import AssetExample from './components/AssetExample';
+import Animated from './components/Animated';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      
+      
+        <AssetExample />
+      <Animated/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'black',
+    padding: 8,
+  },
+ 
+});
